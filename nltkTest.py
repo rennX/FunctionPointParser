bf07f725ba3af86a534b0f25a6aaedacd38106e6
Aaron Skinner <renn.xa@gmail.com>
2014-02-18 20:56:35 -0600

    2nd revision of test code. Chgd data structures.  Now with comments

--- nltkTest.py
+++ nltkTest.py
@@ -2,25 +2,33 @@
 import re
 import nltk
 
-text = """#1The system shall allow users to login to their accounts. [[ILF]]
+#text = """#1The system shall allow users to login to their accounts.The Python interpreter changes the prompt from >>> to ... after encountering the colon at the end of the first line. The ... prompt indicates that Python expects an indented code block to appear next. It is up to you to do the indentation, by typing four spaces or hitting the tab key. To finish the indented block just enter a blank line. [[ILF]]
 #2. The system shall maintain  permissions for what actions a user is permitted to perform. [[ILF]]
 #3.The system shall maintain customized pages based on the user's role. [[ILF]]
-#4 The system shall display a list of all events associated with the attraction's page if the user is an administrator, booking agent, business user, financial user, or the attration's manager agent.  [[EIF]]
-5. The system shall display all the events scheduled at a venue on the venue's page if the user is an administrator, booking agent, business user, or financial user. [[EIF]]
+#4 The system shall display a list of all events associated with the attraction's page if the user is an administrator, booking agent, business user, financial user, or the attration's manager agent.  [[EIF]]5. The system shall display all the events scheduled at a venue on the venue's page if the user is an administrator, booking agent, business user, or financial user. [[EIF]]
 #6. The system shall provide the ability to search the directory for an individual by phone number. [[EO]]"""
+
+text = """#1 one. [[tag]]
+#2 two.
+#3 three.
+#4 four
+#5 five."""
 p = re.compile(r'#?\d[\s\.]?[\s]?')
 out = filter(None, p.split(text))
+print out[2]
+print len(out)
 i=0
 for s in out:
-	print 
 	i += 1
 	text = nltk.word_tokenize(s)
+#	print text
 	pos = nltk.pos_tag(text)
+#	print pos
 	pattern = "NP: {<DT>?<JJ>*<NN>}"
 	NPChunker = nltk.RegexpParser(pattern)
 	result = NPChunker.parse(pos)  
-	print "Chunked POS TAG:::Block " + str(i)
-	print result
+#	print "Chunked POS TAG:::Block " + str(i)
+#	print result
 	
 
 #print "\n".join(out)
---
+++ readInput.py
@@ -0,0 +1,71 @@
+#! /usr/bin/env python
+import re
+import nltk
+import numpy
+
+# this can be its own function
+# it open a text file for reading
+f = open('input.txt')
+input = f.read()
+f.close()
+
+
+# this can be its own function
+# it splits the input text into blocks
+blockRegEx = re.compile(r'##\d[\s\.]?[\s]?')
+out = filter(None, blockRegEx.split(input))
+
+# this can be its own function
+# list is initialized with headings - 
+# then the regex pattern for tags is set
+# then a 2D array is populated with data
+# one block per row
+list = ['block','text','tag']
+tagRegEx = re.compile(r'\[\[(.+?)\]\]')
+i=0
+
+for x in out:
+	i += 1
+	m = re.search(tagRegEx, x)
+	if m:
+	    tag = m.group(1)
+	x = re.sub(tagRegEx,'',x)
+	list2=[str(i),x,tag]
+	list = numpy.vstack([list,list2])
+print len(out)
+print list
+
+# test to check number of rows and columns
+numrows = len(list)
+numcols = len(list[0])
+print "Num Rows: " + str(numrows-1) + ":: Num Cols: " + str(numcols)
+
+# test to print tags column only
+i=0
+for t in list:
+	i += 1
+	print t[2]
+
+# this can be its own function
+# it processes the 2nd column of the 
+# 2D array row by row - for each row 
+# the block is tokenized, tagged, 
+# noun phrases are chunked out
+# the variable result is a text based
+# representation of a tree w/ 'S' as 
+# the head
+i=-1
+for s in list:
+	i += 1
+	text = nltk.word_tokenize(s[1])
+	pos = nltk.pos_tag(text)
+	# regex pattern to define noun phrase
+	pattern = "NP: {<DT>?<JJ>*<NN>}"
+	NPChunker = nltk.RegexpParser(pattern)
+	result = NPChunker.parse(pos)
+	print "Chunked POS TAG:::Block " + str(i)
+	print result
+	
+
+
+
