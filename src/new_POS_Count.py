af120dff2599ab66a4e62deeb81b0ac73578cf37
jnieman <joshnmn@gmail.com>
2014-03-12 20:12:26 -0500

    updated POS Count to include 2D array
    these POS Count functions allow for a list of lists as an argument.  The result comes out funky for indices 2-7...they seem to combine 2 blocks in those indices while index 1, 8, and 9 each contain their own block.

--- src/new_POS_Count.py
+++ src/new_POS_Count.py
@@ -20,56 +20,66 @@ def countNouns( aList ):
 	"""
 	totalNounCount = 0
 	nounCount = 0
-	#print aList
 	findNoun = re.compile('NN')
 	for x in aList:
-		for y in x
+		for y in x:
 			if findNoun.search(str(y)) is not None:
 				nounCount += 1
 		print( nounCount )
 		print( "\n" )
 		totalNounCount += nounCount
 		nounCount = 0
-	#print( nounCount )
 	return totalNounCount
 
 def countVerbs( aList ):
 	"""
-	Takes a list of strings and returns the count of total verbs in list.
+	Takes a list of lists of strings with POS information and returns 
+	the count of total verbs in each sub-list.
 		
 	Args:
-		aList: a list of strings for processing
-				ex.  "text"
+		aList: a list of lists of strings for processing
+				ex.  [["text, VB"],["text2, VB"]]
 
 	Returns:
 		Integer value of the count of total verbs in list
 	"""
+	totalVerbCount = 0
 	verbCount = 0
 	findVerb = re.compile('VB')
 	for x in aList:
-		if findVerb.search(str(x)) is not None:
-			verbCount += 1
-#	print( verbCount )
-	return verbCount
+		for y in x:
+			if findVerb.search(str(xy) is not None:
+				verbCount += 1
+		print( verbCount )
+		print( "\n" )
+		totalVerbCount += verbCount
+		verbCount = 0
+	return totalVerbCount
 
 def countAdjectives( aList ):
 	"""
-	Takes a list of strings and returns the count of total adjectives in list.
+	Takes a list of lists of strings with POS information and returns 
+	the count of total adjectives in each sub-list.
 		
 	Args:
-		aList: a list of strings for processing
-				ex.  "text"
+		aList: a list of lists of strings for processing
+				ex.  [["text, JJ"],["text2, JJ"]]
 
 	Returns:
 		Integer value of the count of total adjectives in list
 	"""
+	totalAdjectiveCount = 0
 	adjectiveCount = 0
 	findAdjective = re.compile('JJ')
 	for x in aList:
-		if findAdjective.search(str(x)) is not None:
-			adjectiveCount += 1
-#	print( adjectiveCount )
-	return adjectiveCount
+		for y in x:
+			if findAdjective.search(str(x)) is not None:
+				adjectiveCount += 1
+		print adjectiveCount
+		print ("\n")
+		totalAdjectiveCount += adjectiveCount
+		adjectiveCount = 0
+	return totalAdjectiveCount
 	
 def main ():
 	"""
@@ -82,25 +92,22 @@ def main ():
 
 	p = re.compile(r'#?\d[\s\.]?[\s]?')
 	out = filter(None, p.split(text))
-	#print out[2]
-	#print len(out)
 	i = 0
+	listOfLists = []
+	
 
 	for s in out:
 		i += 1
 		text = nltk.word_tokenize(s)
-		#print text
 		pos = nltk.pos_tag(text)
-		#print pos
 		pattern = "NP: {<DT>?<JJ>*<NN>}"
 		NPChunker = nltk.RegexpParser(pattern)
 		result = NPChunker.parse(pos)
-		#print "Chunked POS TAG:::Block " + str(i)
+		listOfLists.append( result )
 
-	#print result
-	print "Total nouns = " + str(countNouns( result ))
-	print "Total verbs = " + str(countVerbs( result ))
-	print "Total adjectives = " + str(countAdjectives( result ))
+	print "Noun Count:\n" + str(countNouns( listOfLists ))
+	print "Verb Count:\n" + str(countVerbs( listOfLists ))
+	print "Adjective Count:\n" + str(countAdjectives( listOfLists ))
 	
 	
 if __name__ == '__main__':
