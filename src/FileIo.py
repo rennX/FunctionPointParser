8e7125c7ec10ba86c6e0eb7c722dab0588382e04
Aaron Skinner <renn.xa@gmail.com>
2014-04-04 10:54:41 -0500

    Cleaned up javadocs

--- src/FileIo.py
+++ src/FileIo.py
@@ -1,24 +1,45 @@
 #! /usr/bin/env python
 
-"""This class reads in and processes 
-        input text from a file"""
+"""This class reads in and processes input text from a file"""
 
 class FileIo:
                         
 
     def __init__(self, file):
-        """Opens input file for reading. Saves
-                file to 'inputFile'. Then closes
-                the input file."""
+        """
+        Opens input file for reading. Saves file to 'inputFile'. Then closes the input file.
+        
+        Args:
+            file:  the file to be processed
+
+        Returns:
+            none
+        """
         self.f = open(file)
         self.inputFile = self.f.read()
         self.f.close()
         
     def toString(self):
-        """Prints out the contents of the input file"""
+        """
+        Prints out the contents of the input file as a string
+        
+        Args:
+            none
+
+        Returns:
+            none
+        """
         print str(self.inputFile)
 
     def getFile(self):
-        """Returns the contents of the input file as a string"""
+        """
+        Returns the contents of the input file as a string
+        
+        Args:
+            none
+
+        Returns:
+            The input file that was processed upon instantiation
+        """
         return self.inputFile
 
